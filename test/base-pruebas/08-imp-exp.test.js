40d9b4663bff1067456c5ba55a1d4e1aaeed085b
Daniel galvan <danielricardogt@gmail.com>
2025-04-27 21:02:00 -0500

    :construction: testing part III

--- test/base-pruebas/08-imp-exp.test.js
+++ test/base-pruebas/08-imp-exp.test.js
@@ -1,29 +1,49 @@
-import { getHeroeById } from "../../src/base-pruebas/08-imp-exp";
+import {
+  getHeroeById,
+  getHeroesByOwner,
+} from "../../src/base-pruebas/08-imp-exp";
+import heroes from "../../src/data/heroes";
 
-describe('08 test imple exa functions', () => {
-    test('getHeroeById function should return and hero by id', () => {
+describe("08 test imple exa functions", () => {
+  test("getHeroeById function should return and hero by id", () => {
     //  prepare the test subject
     const id = 1;
     // stimulus
     const hero = getHeroeById(id);
     console.log(hero);
 
-    expect(hero).toEqual( { id: 1, name: 'Batman', owner: 'DC' })
+    expect(hero).toEqual({ id: 1, name: "Batman", owner: "DC" });
+  });
 
-    });
+  test("getHeroeById function should return undefined when id does not exist", () => {
+    //  prepare the test subject
+    const id = 100;
+    // stimulus
+    const hero = getHeroeById(id);
+    // console.log(hero);
+    // observe the expect behavior
+    expect(hero).toBeFalsy();
+  });
 
-    test('getHeroeById function should return undefined when id does not exist', () => {
-        //  prepare the test subject
-        const id = 100;
-        // stimulus
-        const hero = getHeroeById(id);
-        console.log(hero);
-    
-        expect(hero).toBeFalsy();
-    
-        });
+  test("get hero byowner function shuld return an array that containes the given DC owner", () => {
+    // prepare the test subject
+    const ownerDC = "Marvel";
+    // stimulus
+    const heroes = getHeroesByOwner(ownerDC);
+    console.log("this is the owner output", heroes);
+    // observe the expect behavior
+    expect(heroes.length).toBe(2);
+    expect(heroes).toEqual(heroes.filter((heroe) => heroe.owner === ownerDC));
+  });
 
-    
-
-    
+  test("should return an array that contains marvel heroes", () => {
+    // prepare the test subject
+    const ownerDC = "DC";
+    // stimulus
+    const heroes = getHeroesByOwner(ownerDC);
+    console.log("this is the owner output", heroes);
+    // observe the expect behavior
+    expect(heroes.length).toBe(3);
+    expect(heroes).toEqual(heroes.filter((heroe) => heroe.owner === ownerDC));
+  });
 });
